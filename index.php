077007fcb64e60be0c0df2d6fc7413689bc169f6
刘艺 <lysoul@yeah.net>
2016-12-02 18:18:02 +0800

    007
    添加了文章分页符号

--- index.php
+++ index.php
@@ -35,19 +35,29 @@
 			
 			<div class="post" id="post-<?php the_id(); ?>">				
 				<h2><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h2>				
+					
 					<div class="entre">				
 						<?php the_content(); ?>
-						<p class="postmetadata">
-						<?php _e('Filed under&#58;'); ?> <?php the_category(', ') ?> <?php _e('by'); ?> <?php  the_author(); ?><br />
-						<?php comments_popup_link('No Comments &#187;', '1 Comment &#187;', '% Comments &#187;'); ?> <?php edit_post_link('Edit', ' &#124; ', ''); ?>
-						</p>				
-					</div>						                                                                     
+						
+							<p class="postmetadata">
+							<?php _e('Filed under&#58;'); ?> <?php the_category(', ') ?> <?php _e('by'); ?> <?php  the_author(); ?><br />
+							<?php comments_popup_link('No Comments &#187;', '1 Comment &#187;', '% Comments &#187;'); ?> <?php edit_post_link('Edit', ' &#124; ', ''); ?>
+							</p>
+					</div>
 			</div>
+			
 			<?php endwhile; ?>
-				<?php else : ?>
+				
+				<div class="navigation">
+					<?php posts_nav_link(); ?>
+				</div>
+				
+			<?php else : ?>
+			 		
 			 		<div class="post" >
 			 		<h2><?php _e('Not_found'); ?></h2>
-			 		</div>                                                   
+					</div>
+					                                                   
 		<?php endif; ?>
 	</div>  
 	
