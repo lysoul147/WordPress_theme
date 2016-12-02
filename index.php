8350d7f154bad203107462a81cefe80b699660da
刘艺 <lysoul@yeah.net>
2016-12-02 13:35:25 +0800

    0.0.5
    添加了日志元数据

--- index.php
+++ index.php
@@ -36,7 +36,11 @@
 			<div class="post">				
 				<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>				
 					<div class="entre">				
-						<?php the_content(); ?>				
+						<?php the_content(); ?>
+						<p class="postmetadata">
+						<?php _e('Filed under&#58;'); ?> <?php the_category(', ') ?> <?php _e('by'); ?> <?php  the_author(); ?><br />
+						<?php comments_popup_link('No Comments &#187;', '1 Comment &#187;', '% Comments &#187;'); ?> <?php edit_post_link('Edit', ' &#124; ', ''); ?>
+						</p>				
 					</div>						                                                                     
 			</div>
 		<?php endwhile; ?>                                                   
