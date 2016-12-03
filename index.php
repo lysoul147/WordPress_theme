63347eb5630c1e4fecb674909436637ca02e4e72
lysoul'Macbook Pro <liuyi@liuyideMacBook-Pro.local>
2016-12-03 18:50:39 +0800

    010
    添加文章归档链接、生成友情链接、添加部分注释

--- index.php
+++ index.php
@@ -26,10 +26,12 @@
 <body>
 	
 	<div id="header">
-		<h1><a name="topic-3"></a><a href="<?php bloginfo('url'); ?>"><?php bloginfo('name'); ?></a></h1>
+<!--生成博客标题和添加首页链接-->
+		<h1><a name="topic-3" href="<?php bloginfo('url'); ?>"><?php bloginfo('name'); ?></a></h1>
+<!--调用博客信息函数中的博客短描述-->
 		<?php bloginfo('description'); ?>
 	</div>
-	
+<!--生成博客的主循环，检测博客中是否有日志-->	
 	<div id="container">
 	  <?php if(have_posts()) : ?><?php while(have_posts()) : the_post(); ?>          
 			
@@ -43,17 +45,18 @@
 							<?php _e('Filed under&#58;'); ?> <?php the_category(', ') ?> <?php _e('by'); ?> <?php  the_author(); ?><br />
 							<?php comments_popup_link('No Comments &#187;', '1 Comment &#187;', '% Comments &#187;'); ?> <?php edit_post_link('Edit', ' &#124; ', ''); ?>
 							</p>
+							
 					</div>
 			</div>
 			
 			<?php endwhile; ?>
-<!--文章的分页符号--!>				
+<!--文章的分页符号-->				
 				<div class="navigation">
 					<?php posts_nav_link(); ?>
 				</div>
 				
 			<?php else : ?>
-<!--无文章时返回的信息--!>			 		
+<!--无文章时返回的信息-->			 		
 			 		<div class="post" >
 			 		<h2><?php _e('Not_found'); ?></h2>
 					</div>
@@ -62,12 +65,33 @@
 	</div>  
 	<div id="sidebar">
 		<ul>
+<!--调用页面链接列表-->
+			<?php wp_list_pages('depth=3&title_li=<h2>页面</h2>'); ?>
+			
 			<li>
+				
 				<h2><?php _e('Categories'); ?></h2>
+				
 				<ul>
-					<?php wp_list_cats('sort_column=name&optinoncount=1&hierarchical=0'); ?>
+<!--列表调用函数可以自动为每一条分类生成一个<li>标签，无需手动添加-->
+					<?php wp_list_cats('sort_column=name&optinoncount=1&hierarchical=0'); ?>				
 				</ul>
+				
 			</li>
+			
+			<li>
+				
+				<h2><?php _e('Archives'); ?></h2>
+					
+					<ul>
+<!--调用文章归档函数-->
+						<?php wp_get_archives('type=monthly'); ?>
+					</ul>
+					
+			</li>
+<!--调用博客的友情链接列表-->			
+			<?php get_links_list(); ?>
+			
 		</ul>
 	</div>
                                                     
